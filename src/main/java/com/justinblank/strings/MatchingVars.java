0fa89f3db1b43e237354ec7a173f658b1000887d
Justin Blank <justin.blank@gmail.com>
2019-10-27 09:25:26 -0400

    Create helper class to keep track of method vars in compiler

---
+++ src/main/java/com/justinblank/strings/MatchingVars.java
@@ -0,0 +1,13 @@
+package com.justinblank.strings;
+
+class MatchingVars {
+    final int charVar;
+    final int counterVar;
+    final int stateVar;
+
+    MatchingVars(int charVar, int counterVar, int stateVar) {
+        this.charVar = charVar;
+        this.counterVar = counterVar;
+        this.stateVar = stateVar;
+    }
+}
