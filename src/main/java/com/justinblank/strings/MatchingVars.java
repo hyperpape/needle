4dbf6a85110e7eebb89891a5ec36cc285e0bb986
Justin Blank <justin.blank@gmail.com>
2020-06-01 22:01:58 -0400

    Update matchingvars constructors

--- src/main/java/com/justinblank/strings/MatchingVars.java
+++ src/main/java/com/justinblank/strings/MatchingVars.java
@@ -1,6 +1,7 @@
 package com.justinblank.strings;
 
 class MatchingVars {
+    final boolean forwards;
     final int lengthVar;
     final int stringVar;
     final int charVar;
@@ -8,10 +9,20 @@ class MatchingVars {
     final int stateVar;
 
     MatchingVars(int charVar, int counterVar, int stateVar, int lengthVar, int stringVar) {
+        this(true, charVar, counterVar, stateVar, lengthVar, stringVar);
+    }
+
+    private MatchingVars(boolean forwards, int charVar, int counterVar, int stateVar,int lengthVar, int stringVar) {
+        this.forwards = forwards;
         this.charVar = charVar;
         this.counterVar = counterVar;
         this.stateVar = stateVar;
         this.lengthVar = lengthVar;
         this.stringVar = stringVar;
     }
+
+    static MatchingVars backwards(int charVar, int counterVar, int stateVar, int lengthVar, int stringVar) {
+        return new MatchingVars(false, charVar, counterVar, stateVar, lengthVar, stringVar);
+    }
+
 }
