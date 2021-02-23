43d114006579f6c1ee42504f9190fbaa289d9006
Justin Blank <justin.blank@gmail.com>
2021-02-23 17:03:26 -0500

    (test) Improve test error messages for SearchMethodTestUtil

--- src/test/java/com/justinblank/strings/SearchMethodTestUtil.java
+++ src/test/java/com/justinblank/strings/SearchMethodTestUtil.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.*;
 public class SearchMethodTestUtil {
 
     public static void find(SearchMethod method, String s, int start, int end) {
-        assertTrue(method.containedIn(s));
+        assertTrue("Wrong result for containedIn on string=\"" + s + "\"", method.containedIn(s));
         MatchResult result = method.find(s, start, end);
         assertTrue(result.matched);
-        assertTrue(method.find(s, start, end, false).matched);
-        assertTrue(method.matches(s.substring(result.start, result.end)));
+        assertTrue("Failed to find in string=\"" + s + "\", start=" + start + ",end=" + end, method.find(s, start, end, false).matched);
+        assertTrue("Failed to match substring of string=\"" + s + "\", start=" + start + ",end=" + end, method.matches(s.substring(result.start, result.end)));
     }
 
     public static void find(SearchMethod method, String s) {
@@ -34,14 +34,14 @@ public class SearchMethodTestUtil {
     }
 
     public static void match(SearchMethod method, String s) {
-        assertTrue(method.matches(s));
-        assertTrue(method.matcher(s).matches());
-        assertEquals(MatchResult.success(0, s.length()), method.find(s));
+        assertTrue("Failed match for string=\"" + s + "\"", method.matches(s));
+        assertTrue("Failed match for string=\"" + s + "\"", method.matcher(s).matches());
+        assertEquals("Failed find for string='" + s + "'", MatchResult.success(0, s.length()), method.find(s));
         find(method, s, 0, s.length());
     }
 
     public static void match(Pattern pattern, String s) {
-        assertTrue(pattern.matcher(s).matches());
-        assertTrue(pattern.matcher(s).containedIn());
+        assertTrue("Failed match for string=\"" + s + "\"", pattern.matcher(s).matches());
+        assertTrue("Failed containedIn for string=\"" + s + "\"", pattern.matcher(s).containedIn());
     }
 }
