9cc60554a48dc172b97cd799f19b629078603e44
Justin Blank <justin.blank@gmail.com>
2023-05-10 12:00:44 +0100

    Another debug util

--- needle-compiler/src/main/java/com/justinblank/strings/DFADebugUtils.java
+++ needle-compiler/src/main/java/com/justinblank/strings/DFADebugUtils.java
@@ -6,8 +6,20 @@ public class DFADebugUtils {
         System.out.println("State=" + state + ", char=" + encodeChr(c));
     }
 
+    public static void debugStateTransition(int newState) {
+        System.out.println("NewState=" + newState);
+    }
+
     public static void returnWasAccepted(int state) {
         System.out.println("Returning was accepted state=" + state);
+    }
+
+    public static void debugIndexForwards(int index) {
+        System.out.println("Index forwards was " + index);
+    }
+
+    public static void debugCallWasAccepted(int state) {
+        System.out.println("Calling was accepted on " + state);
     }
 
     public static void failedLookAheadCheck(int index, int computedIndex) {
